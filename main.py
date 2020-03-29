1481a31f768552d2730805bdd94fd8af6aac1f2c
Erohal <erohalthebest@gmail.com>
2020-03-29 09:37:45 +0800

    优化

--- main.py
+++ main.py
@@ -1,35 +1,19 @@
+import json
 import requests
+def login(session,url,username,password):
+    login_url = url + '/auth/login'
+    body = {'email':username,'passwd':password}
+    session.post(login_url,data=body)
+def report(session,url):
+    url = url + '/user/checkin'
+    print(json.loads(session.post(url).text))
 
-base_url = 'http://www.gwbn-vpn.top'
-
-
-def checkin():
-    email = input('email: ')
-    password = input('password: ')
-
-    email = email.split('@')
-    email = email[0] + '%40' + email[1]
-
-    session = requests.session()
-    login_url = base_url + '/auth/login'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    }
-    post_data = 'email=' + email + '&passwd=' + password + '&code=&remember_me=week'
-    post_data = post_data.encode()
-    response = session.post(login_url, post_data, headers=headers, verify=False)
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-        'Referer': base_url + '/user'
-    }
-
-    response = session.post(base_url + '/user/checkin', headers=headers)
-
-while True:
-    try:
-        checkin()
-    except Exception:
-        continue
-    break
+def main():
+    base_url = input('url:')
+    email = input('email:')
+    password = input('password:')
+    session = requests.Session()
+    login(session,base_url,email,password)
+    report(session,base_url)
+if __name__ == '__main__':
+    main()
